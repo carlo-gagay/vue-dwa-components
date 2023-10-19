140af641f51b7541208253b5fd16a99cb6df5274
carlo-gagay <tabayagcarlo94@gmail.com>
2023-10-20 07:46:22 +0800

    Updates

--- src/demo/ReadersReadingModeLayoutDemo/ReadersReadingModeLayoutDemo.vue
+++ src/demo/ReadersReadingModeLayoutDemo/ReadersReadingModeLayoutDemo.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Grid from 'vue-virtual-scroll-grid'
 import { defineAsyncComponent, reactive, ref } from 'vue'
 import { ReadersReadingModeLayout } from '@layouts'
 import AsidePanel from './AsidePanel.vue'
@@ -22,6 +23,12 @@ const onAsideShow = () => (asideShown.value = true)
 const onAsideClose = () => (asideShown.value = false)
 const onModalOpen = () => (modal.shown = true)
 const onModalClose = () => (modal.shown = false)
+
+const pageProvider = async (pageNumber, pageSize) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(new Array(pageSize).fill(chapter)), 1000)
+  })
+}
 </script>
 
 <template>
@@ -38,33 +45,48 @@ const onModalClose = () => (modal.shown = false)
         @onAsideClose="onAsideClose"
         @onModalOpen="onModalOpen"
       />
-      <Modal :shown="modal.shown" size="lg" theme="transparent" @onClose="onModalClose">
-        <div class="relative">
-          <div class="column-center text-center text-neutral-white">
-            <div class="text-heading-2">The Glove</div>
-            <div class="text-sub-heading-2">Chapter 3: A Twist of a Knife</div>
+      <Modal :shown="modal.shown" @onClose="onModalClose">
+        <div class="relative p-[27px] w-[1124px]">
+          <div class="heading">
+            <div class="column-center text-center text-neutral-white">
+              <div class="text-heading-2">The Glove</div>
+              <div class="text-sub-heading-2">Chapter 3: A Twist of a Knife</div>
+            </div>
+            <button
+              class="absolute close-btn btn btn-trans-white-inline top-7 -right-8"
+              @click="onModalClose"
+            >
+              <i class="icon icon-close icon--s32"></i>
+            </button>
           </div>
-          <button
-            class="absolute close-btn btn btn-trans-white-inline top-2 -right-12"
-            @click="onModalClose"
-          >
-            <i class="icon icon-close icon--s32"></i>
-          </button>
-        </div>
-        <div class="column-start gap-y-4 mt-[34px] overflow-y-auto h-[475px] rounded-[5px]">
-          <template v-for="(n, index) in 4" :key="n" v-once>
-            <ChapterCardInline
-              :alt="chapter.title"
-              :chapterNumber="`${chapter.chapterNumber}${n}`"
-              :comments="chapter.comments"
-              :description="chapter.description"
-              :image="`${chapter.image}${index}`"
-              :pages="chapter.pages"
-              :progress="chapter.progress"
-              :published="chapter.published"
-              :title="chapter.title"
-            />
-          </template>
+          <div class="mt-[34px] overflow-y-auto h-[475px] rounded-[5px]">
+            <Grid
+              class="grid grid-cols-1 gap-y-4"
+              :length="8"
+              :pageSize="3"
+              :pageProvider="pageProvider"
+              v-once
+            >
+              <template #probe="{ style, index }">
+                <div class="h-[216px]" :style="style"></div>
+              </template>
+              <template #default="{ item, style, index }">
+                <div :style="style" :key="index">
+                  <ChapterCardInline
+                    :alt="item.title"
+                    :chapterNumber="`${item.chapterNumber}${index}`"
+                    :comments="item.comments"
+                    :description="item.description"
+                    :image="`${item.image}${index}`"
+                    :pages="item.pages"
+                    :progress="item.progress"
+                    :published="item.published"
+                    :title="item.title"
+                  />
+                </div>
+              </template>
+            </Grid>
+          </div>
         </div>
       </Modal>
     </template>
